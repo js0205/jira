7d3f85f9d9e3b68d1c986f86575513bb4e3c39b6
js0205 <467353069@qq.com>
2022-10-02 22:31:51 +0800

    chore: any->unknown

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export const isFalsy = (value: any) => (value === 0 ? false : !value);
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 
 //在一个函数里，改变传入的对象本身是不好的
 export const cleanObject = (object: object) => {
@@ -22,8 +22,8 @@ export const useMount = (callback: () => void) => {
     callback();
   }, []);
 };
-
-export const useDebounce = (value: any, delay?: number) => {
+//后面用泛型来规范类型
+export const useDebounce = (value: unknown, delay?: number): any => {
   const [debouncedValue, setDebouncedValue] = useState(value);
   useEffect(() => {
     //每次在value变化以后，设置一个定时器
