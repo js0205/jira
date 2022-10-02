3f82b35bebe6798ba6ec6f173d6f46cc8c3e8780
js0205 <467353069@qq.com>
2022-10-02 14:13:44 +0800

    feat: 添加管理列表

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { SearchPanel } from "./search-panel";
+import { List } from "./list";
+import { useState, useEffect } from "react";
+import { cleanObject } from "../../utils";
+import * as qs from "qs";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const [users, setUsers] = useState([]);
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
+      async (response) => {
+        if (response.ok) {
+          setList(await response.json());
+        }
+      }
+    );
+  }, [param]);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      <SearchPanel param={param} setParam={setParam} users={users} />
+      <List list={list} users={users} />
+    </div>
+  );
+};
