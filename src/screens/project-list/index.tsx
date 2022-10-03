6edc9cfcc0c69fe9c69b881f5bb5277fcee34826
js0205 <467353069@qq.com>
2022-10-04 03:40:17 +0800

    feat: 添加登录状态维持

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -3,8 +3,7 @@ import { SearchPanel } from "./search-panel";
 import { List } from "./list";
 import { useState, useEffect } from "react";
 import { cleanObject, useDebounce, useMount } from "../../utils";
-
-const apiUrl = process.env.REACT_APP_API_URL;
+import { useHttp } from "utils/http";
 
 export const ProjectListScreen = () => {
   const [users, setUsers] = useState([]);
@@ -15,22 +14,13 @@ export const ProjectListScreen = () => {
   const debouncedParam = useDebounce(param, 200);
 
   const [list, setList] = useState([]);
+  const client = useHttp();
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${cleanObject(debouncedParam)}`).then(
-      async (response) => {
-        if (response.ok) {
-          setList(await response.json());
-        }
-      }
-    );
+    client("projects", { data: cleanObject(debouncedParam) }).then(setList);
   }, [debouncedParam]);
 
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async (response) => {
-      if (response.ok) {
-        setUsers(await response.json());
-      }
-    });
+    client("users").then(setUsers);
   });
 
   return (
