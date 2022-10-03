b00021a1ac0b52053eb5e60d5b572680d902fee0
js0205 <467353069@qq.com>
2022-10-03 21:07:39 +0800

    feat: auth-provider

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -19,7 +19,7 @@ const apiUrl = process.env.REACT_APP_API_URL;
 
 export const LoginScreen = () => {
   const login = (param: { username: string; password: string }) => {
-    fetch(`${apiUrl}/login`, {
+    fetch(`${apiUrl}/register`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -49,7 +49,7 @@ export const LoginScreen = () => {
         <label htmlFor="password">密码</label>
         <input type="password" id={"password"} />
       </div>
-      <button type="submit">登录</button>
+      <button type="submit">注册</button>
     </form>
   );
 };
